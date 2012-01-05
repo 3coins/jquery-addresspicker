fb819a0d899a70438e7405f2082b275af121c18e
3coins <pyjain@gmail.com>
2012-01-05 18:46:03 -0500

    added form detection around the autocomplete, submits form only if value entered matches one from the search list

--- src/jquery.ui.addresspicker.js
+++ src/jquery.ui.addresspicker.js
@@ -12,159 +12,198 @@
  *   jquery.ui.widget.js
  *   jquery.ui.autocomplete.js
  */
-(function( $, undefined ) {
+(function($, undefined) {
 
-$.widget( "ui.addresspicker", {
-	options: {
-	  appendAddressString: "",
-		mapOptions: {
-		  zoom: 5, 
-		  center: new google.maps.LatLng(46, 2), 
-		  scrollwheel: false,
-		  mapTypeId: google.maps.MapTypeId.ROADMAP
+	$.widget("ui.addresspicker", {
+		options : {
+			appendAddressString : "",
+			mapOptions : {
+				zoom : 5,
+				center : new google.maps.LatLng(46, 2),
+				scrollwheel : false,
+				mapTypeId : google.maps.MapTypeId.ROADMAP
+			},
+			elements : {
+				map : false,
+				lat : false,
+				lng : false,
+				locality : false,
+				country : false
+			},
+			draggableMarker : true
 		},
-		elements: {
-		  map: false,
-		  lat: false,
-		  lng: false,
-		  locality: false,
-		  country: false
+
+		marker : function() {
+			return this.gmarker;
 		},
-	  draggableMarker: true
-	},
 
-	marker: function() {
-		return this.gmarker;
-	},
-	
-	map: function() {
-	  return this.gmap;
-	},
+		map : function() {
+			return this.gmap;
+		},
 
-  updatePosition: function() {
-    this._updatePosition(this.gmarker.getPosition());
-  },
-  
-  reloadPosition: function() {
-    this.gmarker.setVisible(true);
-    this.gmarker.setPosition(new google.maps.LatLng(this.lat.val(), this.lng.val()));
-    this.gmap.setCenter(this.gmarker.getPosition());
-  },
-  
-  selected: function() {
-    return this.selectedResult;
-  },
-  
-	_create: function() {
-	  this.geocoder = new google.maps.Geocoder();
-	  this.element.autocomplete({
-			source: $.proxy(this._geocode, this),  
-			focus:  $.proxy(this._focusAddress, this),
-			select: $.proxy(this._selectAddress, this)
-		});
-		
-		this.lat      = $(this.options.elements.lat);
-		this.lng      = $(this.options.elements.lng);
-		this.locality = $(this.options.elements.locality);
-		this.country  = $(this.options.elements.country);
-		if (this.options.elements.map) {
-		  this.mapElement = $(this.options.elements.map);
-  		this._initMap();
-		}
-	},
+		updatePosition : function() {
+			this._updatePosition(this.gmarker.getPosition());
+		},
 
-  _initMap: function() {
-    if (this.lat && this.lat.val()) {
-      this.options.mapOptions.center = new google.maps.LatLng(this.lat.val(), this.lng.val());
-    }
+		reloadPosition : function() {
+			this.gmarker.setVisible(true);
+			this.gmarker.setPosition(new google.maps.LatLng(this.lat.val(),
+					this.lng.val()));
+			this.gmap.setCenter(this.gmarker.getPosition());
+		},
 
-    this.gmap = new google.maps.Map(this.mapElement[0], this.options.mapOptions);
-    this.gmarker = new google.maps.Marker({
-      position: this.options.mapOptions.center, 
-      map:this.gmap, 
-      draggable: this.options.draggableMarker});
-    google.maps.event.addListener(this.gmarker, 'dragend', $.proxy(this._markerMoved, this));
-    this.gmarker.setVisible(false);
-  },
-  
-  _updatePosition: function(location) {
-    if (this.lat) {
-      this.lat.val(location.lat());
-    }
-    if (this.lng) {
-      this.lng.val(location.lng());
-    }
-  },
-  
-  _markerMoved: function() {
-    this._updatePosition(this.gmarker.getPosition());
-  },
-  
-  // Autocomplete source method: fill its suggests with google geocoder results
-  _geocode: function(request, response) {
-    var address = request.term, self = this;
-    this.geocoder.geocode( { 'address': address + this.options.appendAddressString}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-          results[i].label =  results[i].formatted_address;
-        };
-      } 
-      response(results);
-    })
-  },
-  
-  _findInfo: function(result, type) {
-    for (var i = 0; i < result.address_components.length; i++) {
-      var component = result.address_components[i];
-      if (component.types.indexOf(type) !=-1) {
-        return component.long_name;
-      }
-    }
-    return false;
-  },
-  
-  _focusAddress: function(event, ui) {
-    var address = ui.item;
-    if (!address) {
-      return;
-    }
-    
-    if (this.gmarker) {
-      this.gmarker.setPosition(address.geometry.location);
-      this.gmarker.setVisible(true);
+		selected : function() {
+			return this.selectedResult;
+		},
 
-      this.gmap.fitBounds(address.geometry.viewport);
-    }
-    this._updatePosition(address.geometry.location);
-    
-    if (this.locality) {
-      this.locality.val(this._findInfo(address, 'locality'));
-    }
-    if (this.country) {
-      this.country.val(this._findInfo(address, 'country'));
-    }
-  },
-  
-  _selectAddress: function(event, ui) {
-    this.selectedResult = ui.item;
-  }
-});
+		_create : function() {
+			this.geocoder = new google.maps.Geocoder();
+			var ac = this.element.autocomplete({
+				source : $.proxy(this._geocode, this),
+				focus : $.proxy(this._focusAddress, this),
+				select : $.proxy(this._selectAddress, this)
+			});
 
-$.extend( $.ui.addresspicker, {
-	version: "@VERSION"
-});
-
-
-// make IE think it doesn't suck
-if(!Array.indexOf){
-	Array.prototype.indexOf = function(obj){
-		for(var i=0; i<this.length; i++){
-			if(this[i]==obj){
-				return i;
+			this.lat = $(this.options.elements.lat);
+			this.lng = $(this.options.elements.lng);
+			this.locality = $(this.options.elements.locality);
+			this.country = $(this.options.elements.country);
+			if (this.options.elements.map) {
+				this.mapElement = $(this.options.elements.map);
+				this._initMap();
 			}
-		}
-		return -1;
-	}
-}
+			
+			// check if this input has a parent form
+			this._checkforForm();
+		},
+		
+		_checkforForm : function() {
+			var form = this.element.closest("form");
+			if(form){
+				form.submit($.proxy(this._checkValue, this));
+			}
+		},
+		
+		_checkValue: function(e){
+			
+			var searchTerm = this.element.val().trim();
+			
+			if(!searchTerm || !this.content){
+				return false;
+			}
+			
+			var matches = $.grep(this.content, function(ele){
+				return ele.label === searchTerm;
+			});
+			
+			setTimeout(function(){
+				// can also show a modal dialog here.
+				alert("Search term not selected from list.");
+			}, 100);
+			
+			return !!matches.length;
+		},
 
-})( jQuery );
+		_initMap : function() {
+			if (this.lat && this.lat.val()) {
+				this.options.mapOptions.center = new google.maps.LatLng(
+						this.lat.val(), this.lng.val());
+			}
+
+			this.gmap = new google.maps.Map(this.mapElement[0],
+					this.options.mapOptions);
+			this.gmarker = new google.maps.Marker({
+				position : this.options.mapOptions.center,
+				map : this.gmap,
+				draggable : this.options.draggableMarker
+			});
+			google.maps.event.addListener(this.gmarker, 'dragend', $.proxy(
+					this._markerMoved, this));
+			this.gmarker.setVisible(false);
+		},
+
+		_updatePosition : function(location) {
+			if (this.lat) {
+				this.lat.val(location.lat());
+			}
+			if (this.lng) {
+				this.lng.val(location.lng());
+			}
+		},
+
+		_markerMoved : function() {
+			this._updatePosition(this.gmarker.getPosition());
+		},
+
+		// Autocomplete source method: fill its suggests with google geocoder
+		// results
+		_geocode : function(request, response) {
+			var address = request.term, self = this;
+			this.geocoder.geocode({
+				'address' : address + this.options.appendAddressString
+			}, function(results, status) {
+				if (status == google.maps.GeocoderStatus.OK) {
+					for ( var i = 0; i < results.length; i++) {
+						results[i].label = results[i].formatted_address;
+					}
+					;
+				}
+				self.content = results;
+				response(results);
+			})
+		},
+
+		_findInfo : function(result, type) {
+			for ( var i = 0; i < result.address_components.length; i++) {
+				var component = result.address_components[i];
+				if (component.types.indexOf(type) != -1) {
+					return component.long_name;
+				}
+			}
+			return false;
+		},
+
+		_focusAddress : function(event, ui) {
+			var address = ui.item;
+			if (!address) {
+				return;
+			}
+
+			if (this.gmarker) {
+				this.gmarker.setPosition(address.geometry.location);
+				this.gmarker.setVisible(true);
+
+				this.gmap.fitBounds(address.geometry.viewport);
+			}
+			this._updatePosition(address.geometry.location);
+
+			if (this.locality) {
+				this.locality.val(this._findInfo(address, 'locality'));
+			}
+			if (this.country) {
+				this.country.val(this._findInfo(address, 'country'));
+			}
+		},
+
+		_selectAddress : function(event, ui) {
+			this.selectedResult = ui.item;
+		}
+	});
+
+	$.extend($.ui.addresspicker, {
+		version : "@VERSION"
+	});
+
+	// make IE think it doesn't suck
+	if (!Array.indexOf) {
+		Array.prototype.indexOf = function(obj) {
+			for ( var i = 0; i < this.length; i++) {
+				if (this[i] == obj) {
+					return i;
+				}
+			}
+			return -1;
+		}
+	}
+
+})(jQuery);
